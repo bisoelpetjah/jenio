eadc8da170f745dd998ecb34134ee7d085867e5e
Irvan Adhitya <irvan@salestock.id>
2016-09-15 17:15:21 +0700

    implement permission checking

--- app/src/main/kotlin/com/cekiboy/ceki/activities/ScanActivity.kt
+++ app/src/main/kotlin/com/cekiboy/ceki/activities/ScanActivity.kt
@@ -1,6 +1,10 @@
 package com.cekiboy.ceki.activities
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.Toolbar
 import android.view.MenuItem
@@ -14,6 +18,8 @@ import me.dm7.barcodescanner.zbar.ZBarScannerView
  * Created by irvan on 9/15/16.
  */
 class ScanActivity: AppCompatActivity(), ZBarScannerView.ResultHandler {
+
+    private val PERMISSION_CAMERA = 10101;
 
     private var toolbar: Toolbar? = null
     private var scannerView: ZBarScannerView? = null
@@ -31,6 +37,10 @@ class ScanActivity: AppCompatActivity(), ZBarScannerView.ResultHandler {
 
         scannerView?.setFormats(arrayListOf(BarcodeFormat.QRCODE))
         scannerView?.setResultHandler(this)
+
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), PERMISSION_CAMERA)
+        }
     }
 
     override fun onResume() {
@@ -41,6 +51,17 @@ class ScanActivity: AppCompatActivity(), ZBarScannerView.ResultHandler {
     override fun onPause() {
         super.onPause()
         scannerView?.stopCamera()
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_CAMERA -> {
+                if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    onBackPressed()
+                }
+            }
+        }
     }
 
     override fun onOptionsItemSelected(item: MenuItem?): Boolean {
