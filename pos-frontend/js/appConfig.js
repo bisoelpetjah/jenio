e3b16da672c02e493e07705593e8850c652850cf
ananta pandu <anpandumail@gmail.com>
2017-08-12 17:04:41 +0700

    [frontend] bugfix base url

--- pos-frontend/js/appConfig.js
+++ pos-frontend/js/appConfig.js
@@ -1,4 +1,4 @@
 export default {
-    baseApiUrl: process.env.BASE_API_URL || 'localhost:1337',
+    baseApiUrl: process.env.BASE_API_URL || 'http://139.59.120.226:1337',
     merchantId: '57eca26dd4050d13616131ae'
 }
